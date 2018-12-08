fc42fd6c32807be4a58ce2db9d5074b9a4fdeb2d
John Nelson Kane <johnnelsonkane@Johns-iMac.local>
2018-12-08 11:00:46 -0700

    Cleaned up file structure

--- Src/Tracking/Object_Location_Model.py
+++ Src/Tracking/Object_Location_Model.py
@@ -1,50 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun  20 13:52:39 2018
+Created on 11/29/2018
 
 @author: Nelly
 
-This class will predict the probable location of an object in frame t+1
-from knowledge of the object location in frame t, the direction of intra-frame
+This class will predict the probable location of an object in frame t2
+from knowledge of the object location in frame t1, the direction of intra-frame
 camera travel, the line-of-sight and various optical and sensor parameters. 
 This model will be used in intraframe tracking of objects, by aiding in the 
 association of object with previous tracks. 
+
+MODIFIED: This version of the object location model differs from the original 
+    in that it used the pitch and the heading to calculate the pixel associated 
+    with the direction vector. 
+    
+heading: indicates the compass heading of the camera. Accepted values are from
+ 0 to 360 (both values indicating North, with 90 indicating East, and 180 South).
+ 
+pitch: (default is 0) specifies the up or down angle of the camera relative to 
+the Street View vehicle. This is often, but not always, flat horizontal. 
+Positive values angle the camera up (with 90 degrees indicating straight up); 
+negative values angle the camera down (with -90 indicating straight down).
+
+CLASS DEFINATIONS:
+    cameraData:
+        LatLonEl: [Latitude (deg), Longitude (deg), Elevation(m)] -> List
+        heading:  heading of image (deg or rad)
+        pitch:    pitch of image (deg or rad)
 
 INPUT:
     (TO CLASS) W: estimated closest point of desired target to direction vector
         in ECEF coordinates (meters). Each object must be instantiated with W
         from this class.
+        
     objRowCol: object row and column from frame t+1
-        (1x2 list)
-    frame_t1_ecef: camera coordinates for frame t in the rotated cartesian
-        coordinate system
-        (1x3 array)
-    frame_t2_ecef: camera coordinates for frame t+1 in the rotated cartesian
-        coordinate system
-        (1x3 array)
-    LOS: line-of-sight-vector in ECEF coordinates
-        (1x3 array)
-    LatLon: Lattitude and longitude of camera for frame_t1, used to orient 
-        camera rotation in 3D space.
-        (1x2 list)
+        ( 1x2 list )
+        
+    camData1: cameraData instance for frame 1.
+    
+    camData2: cameraData instance for frame 2.
+    
+    degFlag: True is camData heading and pitch in degrees.
         
 OUTPUT:
     [ROW, COL] of the predicted position of an object in frame t+1
         (1x2 list)
-    
-ASSUMPTIONS:
-    - LOS vector is constant from frame to frame. 
-    - Direction vector must be within the field of view.
 
-TO DO's: 
-    - move the estimated 'closest dist. to object' from defined constant to
-    a class input. Also, determine how this can be a variable, not const.
-    
+ASSUMPTIONS: 
+    - Constant Field-Of-View (FOV)
+    - All objects detected with the same instance have same 'W' estimation. For
+      applications with multiple 'W' applications, have multiple 'W's.
+
+TODO: 
+    - Currently using W (closest dist of obj. to D vector estimate) as a param
+    that is held within the class. This may frequently change and will need to
+    be variable once this class is used for more general targets. 
 """
+import sys, os
+sys.path.append(os.path.join(sys.path[0],'..','Utilities'))
 import numpy as np
+from enu_to_ecef import enu2ecef
+from rotation import Rotate
+from coord_transfers import CoordTransfers
+from latGIS_containers import CameraData        
+  
 class Object_Location_Model:
     
-    def __init__(self, W):
+    def __init__(self, W: float):
         
         # constants
         self.FOV = 30 # degrees
@@ -59,26 +81,52 @@ class Object_Location_Model:
         self.W_est = W # (m) : estimated closest dist. to obj. from D vector.
         
     
-    def objectLocationPredictor(self, objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon):
+    def objectLocationPredictor(self, objRowCol: list, camData1: CameraData, camData2: CameraData, degFlag : bool = False )-> list:
         
         '''This function will be used to predict the objects location in frame t+1 
         from from knowledge of frame t, various camera parameters and the necessary
         geometrical arguments.
         INPUT: 
-            - objRowCol
-            - frame_t1_ecef
-            - frame_t2_ecef
-            - LOS
-            - LatLon
+            - objRowCol ( in frame 1 )
+            - camData1 ( degrees )
+            - camData2 ( degrees )
+            - degFlag ( degrees )
+
         '''
+        # TODO: this should be moved
+        coordTransfers = CoordTransfers() 
+        # transfer Latitude, Longitude to ECEF, converted to arrays
+        frame_t1_ecef = np.asarray(coordTransfers.LLE_to_ECEF(LLE = camData1.LatLonEl))
+        frame_t2_ecef = np.asarray(coordTransfers.LLE_to_ECEF(LLE = camData2.LatLonEl))
+        
+        # degrees to radians, if (degFlag == True)
+        if (degFlag == 1):    
+            heading_t1 = camData1.heading*self.deg2rad
+            pitch_t1 = camData1.pitch*self.deg2rad
+            heading_t2 = camData2.heading*self.deg2rad
+            pitch_t2 = camData2.pitch*self.deg2rad
+        else:
+            heading_t1 = camData1.heading
+            pitch_t1 = camData1.pitch
+            heading_t2 = camData2.heading
+            pitch_t2 = camData2.pitch
         
         # distance moved along direction vector
         deltaL = np.sqrt((frame_t2_ecef[0] - frame_t1_ecef[0])**2 + \
                          (frame_t2_ecef[1] - frame_t1_ecef[1])**2 + \
                          (frame_t2_ecef[2] - frame_t1_ecef[2])**2)
-        # get direction of movement in camera coordinates
-        [dirRow, dirCol] = self.getDirectionPixel(frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
-        directionPixel = [dirRow, dirCol]
+        
+        # if the camera has not moved place the direction pixel in the center of the image
+        if (deltaL == 0):
+            directionPixel = [self.sensorSize[0]/2, self.sensorSize[1]/2]
+        else:
+            # get direction of movement in camera coordinates
+            directionPixel = self.getDirectionPixel(frame_t1_ecef, frame_t2_ecef, heading_t1,
+                                                    pitch_t1, camData1.LatLonEl)
+
+        # TODO: solve issue if heading is close to 360 degrees
+    
+        
         PIXEL_t2 = [[],[]]
         for sensorDir in [0,1]:
         
@@ -100,91 +148,109 @@ class Object_Location_Model:
             # since direction vector is within the FOV, the new length must be 
             # shorter than the previous length.
             L_t2 = L_est - deltaL
-            objAngle_t2 = np.arctan(self.W_est/L_t2)
-            pixelsFromD = self.focalLength*np.tan(objAngle_t2)
-            #calculate final position ob object in pixel values
-            PIXEL_t2[sensorDir] = directionPixel[sensorDir] + sign*pixelsFromD
+            # if we have passed to object and the new L is negative, the angle
+            # may be nagative and will need to be adjusted by PI.
+            if (L_t2 < 0):
+                objAngle_t2 = np.pi - np.abs(np.arctan(self.W_est/L_t2))
+            else:
+                objAngle_t2 = np.abs(np.arctan(self.W_est/L_t2))
             
-       
+            # correct for heading and pitch
+            if (sensorDir == 0):
+                # add to obj angle before computing pixel offset
+                # NOTE: The sign of this value represents object pixel > direction pixel
+                sensorOrientCorrectAng = sign*objAngle_t2 + (pitch_t2 - pitch_t1)
+            
+                
+            else:
+                sensorOrientCorrectAng = sign*objAngle_t2 + (heading_t1 - heading_t2)
+            
+            # if angle from LOS to object if greater than pi/2 object cannot even
+            # project onto the same plane as the focal plane.
+            if (sensorOrientCorrectAng > np.pi/2):
+                sensorOrientCorrect = np.nan
+            else:
+                #calculate final position ob object in pixel values
+                sensorOrientCorrectAngSign = np.sign(sensorOrientCorrectAng)
+                sensorOrientCorrect = (sensorOrientCorrectAngSign*self.focalLength*
+                                       np.tan(np.abs(sensorOrientCorrectAng)))
+                
+            PIXEL_t2[sensorDir] = directionPixel[sensorDir] + sensorOrientCorrect
+              
         return PIXEL_t2
     
 
-    def getDirectionPixel(self, frame_t1_ecef, frame_t2_ecef, LOS, LatLon):
+    def getDirectionPixel(self, frame_t1_ecef: np.ndarray, frame_t2_ecef: np.ndarray,
+                          heading: float, pitch: float, LatLonEl: list)-> list:
         
         '''This function will calculate to direction of the camera movement in
         pixel coordinates.
         INPUT:
             - frame_t1_ecef
             - frame_t2_ecef
-            - LOS
-            - LatLon
+            - heading
+            - pitch
+            - LatLonEl
+        OUTPUT:
+            - direction pixel [ row , col ]
             '''
-        # calculating virtual focal length, only if not calculated yet
-        self.focalLength = self.calcVirtualFocalLength()
+        
+        # get normal of earth at current frame t
+        Normal = self.calculateNormal(LatLonEl[0:2])
+        
+        # get North vector at current frame t in ECEF
+        North = self.getNorthECEF(LatLonEl)
+        
+        # rotate North about Normal by heading degrees to get LOS, zero pitch
+        # NOTE: Since the current rotation routine uses two points rather than a
+        # vector to define the rotation axes we will solve for two points on
+        # Normal, p0: ECEF coord of camera, and p1: ECEF coord of camera + some 
+        # distance along the Normal. p0 is frame_t1_ecef
+        p1x = frame_t1_ecef[0] + 1000*Normal[0]
+        p1y = frame_t1_ecef[1] + 1000*Normal[1]
+        p1z = frame_t1_ecef[2] + 1000*Normal[2]
+        # TODO: There is an error here with certain rotations
+        LOS_noPitch = Rotate(North, frame_t1_ecef, [p1x, p1y, p1z], heading) 
+        LOS_noPitch = LOS_noPitch/np.linalg.norm(LOS_noPitch)
+        
         # calculation direction vector in ECEF
         Dvec = frame_t2_ecef - frame_t1_ecef
         Dvec = Dvec/np.linalg.norm(Dvec)
-        # find angle between LOS and D
-        D_LOS_angle = self.angleBetweenVectors(LOS, Dvec)
-        # find total offset in pixel values (direction of offset unknown at this time)
-        totalOffset = self.calcPixelOffset(D_LOS_angle)
-        # calculate normal of earth at current frame t
-        normal = self.calculateNormal(LatLon)
-        # rotate LOS and Dvec to coord system whose normal to earth would be 
-        # N_tilda = <0,0,1>
-        Dvec_tilda = self.rotateCoordsToTilda(Dvec, normal)
-        LOS_tilda = self.rotateCoordsToTilda(LOS, normal)
         
-        # find angle between D and (X_tilda, Y_tilda) plane
-        # NOTE: to find, we measure from N_tilda = <0,0,1>
-        D_xy_angle = np.pi/2 - self.angleBetweenVectors(Dvec_tilda, np.array((0,0,1)))
-        
-        # find angle between LOS and (X_tilda, Y_tilda) plane
-        # NOTE: to find, we measure from N_tilda = <0,0,1>
-        LOS_xy_angle = np.pi/2 - self.angleBetweenVectors(LOS_tilda, np.array((0,0,1)))
-        
-        # find difference from angles above to get row offset
-        D_LOS_angleRow = D_xy_angle - LOS_xy_angle
-        
-        # this value is absolute, direction of offset is not determined here
-        D_rowOffset = self.calcPixelOffset(D_LOS_angleRow)
+        # find angle between LOS (no pitch) and Direction vector
+        D_LOS_angle = self.angleBetweenVectors(LOS_noPitch, Dvec)
+
+        # find column offset in pixel values (direction of offset unknown at this time)
+        columnOffset = self.calcPixelOffset(D_LOS_angle)
+
         # calculate direction of offset with angle comparison against the normal
-        if (D_LOS_angleRow > 0):
-            ROW = np.floor(self.sensorSize[0])/2 - D_rowOffset
+        vecTemp = np.cross(Dvec, LOS_noPitch)
+        angleTemp = self.angleBetweenVectors(vecTemp, Normal)
+        
+        if (angleTemp > np.pi/2):
+            COL = np.floor(self.sensorSize[1])/2 - columnOffset
         else:
-            ROW = np.floor(self.sensorSize[0])/2 + D_rowOffset
+            COL = np.floor(self.sensorSize[1])/2 + columnOffset
          
-        SMALL_NUMBER = 0.00001   
-        if (D_rowOffset - totalOffset > SMALL_NUMBER):
-            print('ERROR: Sensor Model: Row offset calculated as greater than total offset')   
-            return[[],[]]
+        # use pitch directly to get ROW
+        if (pitch != 0): 
+            rowOffset = self.calcPixelOffset(np.abs(pitch))
+            if (pitch > 0):
+                ROW = np.floor(self.sensorSize[0])/2 + rowOffset
+            else:
+                ROW = np.floor(self.sensorSize[0])/2 - rowOffset
             
-        # use Pythagorean's theorem to get column offset 
-        if (totalOffset > SMALL_NUMBER) and ((totalOffset - D_rowOffset)>0):
-            D_colOffset = np.sqrt(totalOffset**2 - D_rowOffset**2)
         else:
-            D_colOffset = 0
-            
-        # calculate direction of offset with cross product
-        vecTemp = np.cross(Dvec_tilda, LOS_tilda)
-        if (vecTemp[2] > 0):
-            COL = np.floor(self.sensorSize[1])/2 + D_colOffset
-        else:
-            COL = np.floor(self.sensorSize[1])/2 - D_colOffset
-            
-        if (ROW < 0 or ROW > self.sensorSize[0]):
-            print('WARNING: Sensor Model: Row prediction off sensor')
-            
-        if (COL < 0 or COL > self.sensorSize[1]):
-            print('WARNING: Sensor Model: Col prediction off sensor')
+            ROW = np.floor(self.sensorSize[0])/2
             
         return [ROW, COL]
     
-    def calculateNormal(self, LatLon):
+    
+    def calculateNormal(self, LatLonEl):
         
         # LatLon should be 1x2 list
-        Lat = self.deg2rad*LatLon[0]
-        Lon = self.deg2rad*LatLon[1]
+        Lat = self.deg2rad*LatLonEl[0]
+        Lon = self.deg2rad*LatLonEl[1]
         
         normal = np.zeros((3,))
         normal[0] = np.cos(Lat)*np.cos(Lon)
@@ -199,68 +265,15 @@ class Object_Location_Model:
         angle = np.arccos((np.dot(vec1,vec2))/(np.linalg.norm(vec1)*np.linalg.norm(vec2)))
         return angle
     
-    '''normal should rotate to <0, 0, 1>'''
-    def rotateCoordsToTilda(self, coordsForRot, normal):
-        
-        ''' This function will rotate a coordinate vector to a system such that 
-        the normal of the earth in the new system is <0,0,1>. This is used to 
-        orient the camera in 3D space, since the assumption must be made that
-        rows of the sensor are parallel to the earth while columns are orthogonal.
-        
-        '''
-        
-        # force normal to be unit vector 
-        normal = normal/np.linalg.norm(normal)
-        # force coordsForRot to be 3x1 for matrix multiplcations
-        if coordsForRot.shape[0] != 3:
-            print('Coordinates must be passed as a 3x1 or 3x0 array in Sensor Model')
-            return []
-        
-        # force normal to be 3x1 for matrix multiplcations
-        if normal.shape[0] != 3:
-            print('Normal vector must be passed as a 3x1 or 3x0 array in Sensor Model')
-            return []
-        
-        # if normal is already (1,0,0) then pass back the input coords
-        if (normal[0] == 0 and normal[1] == 0 and normal[2]>0):
-            return coordsForRot
-        
-        # find rotation angle about X
-        angleX = np.arctan2(normal[1],normal[2])
-        
-        # rotation matrix about X
-        Rx = np.array([[1, 0, 0], [0, np.cos(angleX), -np.sin(angleX)], \
-                       [0, np.sin(angleX), np.cos(angleX)]])
-        
-        # temporary coords
-        coordsTemp = np.matmul(Rx, coordsForRot)
-        normalTemp = np.matmul(Rx, normal)
-        
-        # find rotation angle about Y
-        angleY = -np.arctan2(normalTemp[0],normalTemp[2])
-    
-        Ry = np.array([[np.cos(angleY), 0, np.sin(angleY)], [0, 1, 0], \
-                        [-np.sin(angleY), 0, np.cos(angleY)]])
-    
-        # rotations    
-        coordsRotated = np.matmul(Ry, coordsTemp)
-        normalRotated = np.matmul(Ry, normalTemp)
-        
-        # check to make sure that new normal is within error to <0, 0, 1>
-        if (abs(normalRotated[0])>0.01 or abs(normalRotated[1])>0.01 or \
-            abs(normalRotated[2] - 1)>0.01):
-            print('WARNING: Normal = ' + str(normalRotated) + \
-                  ', Normal should be [0, 0, 1]')
-        
-        return coordsRotated
     
     def calcVirtualFocalLength(self):
         '''Pixel units'''
         FOV = self.FOV
         # focal length calculation is based off 2nd sensor size value, numCols
         numCols = self.sensorSize[1]
-        focalLength = numCols/(2*np.arctan(FOV/2))
+        focalLength = numCols/(2*np.arctan(self.deg2rad*FOV/2))
         return focalLength
+    
     
     def calcPixelOffset(self, angle):
         
@@ -269,14 +282,15 @@ class Object_Location_Model:
         offset = (self.focalLength)*np.tan(angle)
         return offset
     
-        
-        
-        
     
-    
+    def getNorthECEF(self, LatLonEl: list) -> np.ndarray:
         
-          
-    
-    
+        # create p0, origin of ENU in ECEF
+        p0x, p0y, p0z = enu2ecef(0, 0, 0, LatLonEl[0], LatLonEl[1], LatLonEl[2])
+        # create p1, point on North axis of ENU in ECEF
+        p1x, p1y, p1z = enu2ecef(0, 1000, 0, LatLonEl[0], LatLonEl[1], LatLonEl[2])
         
-    
+        northECEF = np.array([(p1x - p0x), (p1y - p0y), (p1z - p0z)])
+        #normalize
+        return northECEF # turn on to normalize / np.linalg.norm(northECEF)
+        
--- Test/Tracking/Object_Location_Model_unitTest.py
+++ Test/Tracking/Object_Location_Model_unitTest.py
@@ -24,15 +24,44 @@ from coord_transfers import CoordTransfers
 CT = CoordTransfers()
 # instantiate the class
 from Object_Location_Model import Object_Location_Model
-locModel = Object_Location_Model(8)
+from  latGIS_containers import CameraData
 
-print('TEST 1: Object along direction of travel')
+def unitTest(expression: bool):
+    if (expression == True):
+        print("TEST PASSED")
+    else:
+        print("TEST FAILED")
+
+'''
+class cameraData:
+    def __init__(self, LatLonEl: list, heading: float, pitch: float):
+        # instance variable unique to each instance
+        self.LatLonEl = LatLonEl   
+        self.heading = heading
+        self.pitch = pitch
+    
+'''
+'''
+IMPORT CALL:
+    (self, objRowCol: list, camData1: cameraData, camData2: cameraData, degFlag : bool = False )-> list:
+'''
+print('TEST 1: Object along direction of travel (prime meridian')
+
 objRowCol = [512, 512]
-frame_t1_ecef = np.asarray([0, 0, 1000])
-frame_t2_ecef = np.asarray([0, 10, 1000])
-LOS = np.asarray([0, 10, 0])
-LatLon = [90, 0]
-PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
+
+locModel = Object_Location_Model(2)
+# We will start in ECEF for distance and convert to LLE for function input.
+ecef1 = np.array((6378137, 0, 0)) # meters
+ecef2 = np.array((6378137, 0, 50)) # meters, due north by 5 meters
+LLE1 = CT.ECEF_to_LLE(ecef1)
+LLE2 = CT.ECEF_to_LLE(ecef2)
+
+cam1 = CameraData(LatLonEl = LLE1, heading = 0, pitch = 0)
+cam2 = CameraData(LatLonEl = LLE2, heading = 0, pitch = 0)
+
+PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, cam1, cam2, degFlag = True)
+unitTest(np.abs(PIXELS_OUT[0] - objRowCol[0]) < 1)
+unitTest(np.abs(PIXELS_OUT[1] - objRowCol[1]) < 1)
 plt.figure()
 v = [1, 1024, 1, 1024]
 plt.axis(v)
@@ -44,15 +73,29 @@ plt.legend(['Object t1', 'Object t2'])
 plt.xlabel('COLUMN')
 plt.ylabel('ROW')
 plt.show()
+
 print('Location Prediction' + str(PIXELS_OUT))
 
-print('TEST 2: ')
-objRowCol = [512, 512]
-frame_t1_ecef = np.asarray([0, 0, 1000])
-frame_t2_ecef = np.asarray([0, 10, 1000])
-LOS = np.asarray([0, 10, -5])
-LatLon = [90, 0]
-PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
+# =====================================================================================
+print('TEST 2: Hand calculated pixels for a regular step')
+
+locModel = Object_Location_Model(1)
+objRowCol = [454, 454]
+
+# We will start in ECEF for distance and convert to LLE for function input.
+ecef1 = np.array((6378137, 0, 0)) # meters
+ecef2 = np.array((6378137, 0, 30)) # meters, due north by 5 meters
+LLE1 = CT.ECEF_to_LLE(ecef1)
+LLE2 = CT.ECEF_to_LLE(ecef2)
+
+cam1 = CameraData(LatLonEl = LLE1, heading = 0, pitch = 0)
+cam2 = CameraData(LatLonEl = LLE2, heading = 0, pitch = 0)
+
+PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, cam1, cam2, degFlag = True)
+
+unitTest(np.abs(PIXELS_OUT[0] - 66) < 1)
+unitTest(np.abs(PIXELS_OUT[1] - 66) < 1)
+print('Predicted Pixel: ' + str(PIXELS_OUT[0]) + ' , '+ str(PIXELS_OUT[1]))
 plt.figure()
 v = [1, 1024, 1, 1024]
 plt.axis(v)
@@ -64,15 +107,27 @@ plt.legend(['Object t1', 'Object t2'])
 plt.xlabel('COLUMN')
 plt.ylabel('ROW')
 plt.show()
-print('Location Prediction' + str(PIXELS_OUT))
 
-print('TEST 3: ')
+# =====================================================================================
+print('TEST 3: One more boresite test, nonzero lats and longs')
+
+locModel = Object_Location_Model(1)
 objRowCol = [512, 512]
-frame_t1_ecef = np.asarray([0, 0, 1000])
-frame_t2_ecef = np.asarray([0, 10, 1000])
-LOS = np.asarray([0, 10, 5])
-LatLon = [90, 0]
-PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
+
+# We will start in ECEF for distance and convert to LLE for function input.
+ecef1 = np.array((3.9054825307866509e-10, 6378137.0, 0.0)) # meters
+ecef2 = np.array((3.9054825307866509e-10+30, 6378137.0, 0.0)) # meters
+LLE1 = CT.ECEF_to_LLE(ecef1)
+LLE2 = CT.ECEF_to_LLE(ecef2)
+
+cam1 = CameraData(LatLonEl = LLE1, heading = 270, pitch = 0)
+cam2 = CameraData(LatLonEl = LLE2, heading = 270, pitch = 0)
+
+PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, cam1, cam2, degFlag = True)
+
+unitTest(np.abs(PIXELS_OUT[0] - objRowCol[0]) < 1)
+unitTest(np.abs(PIXELS_OUT[1] - objRowCol[1]) < 1)
+print('Predicted Pixel: ' + str(PIXELS_OUT[0]) + ' , '+ str(PIXELS_OUT[1]))
 plt.figure()
 v = [1, 1024, 1, 1024]
 plt.axis(v)
@@ -84,44 +139,3 @@ plt.legend(['Object t1', 'Object t2'])
 plt.xlabel('COLUMN')
 plt.ylabel('ROW')
 plt.show()
-print('Location Prediction' + str(PIXELS_OUT))
-
-print('TEST 4: ')
-objRowCol = [512, 512]
-frame_t1_ecef = np.asarray([0, 0, 1000])
-frame_t2_ecef = np.asarray([0, 10, 1000])
-LOS = np.asarray([-5, 10, 5])
-LatLon = [90, 0]
-PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
-plt.figure()
-v = [1, 1024, 1, 1024]
-plt.axis(v)
-# maintaining camera coordinate system
-plt.ylim(1024,1)
-plt.scatter(objRowCol[1], objRowCol[0], color = 'b')
-plt.scatter(PIXELS_OUT[1], PIXELS_OUT[0], color = 'r')
-plt.legend(['Object t1', 'Object t2'])
-plt.xlabel('COLUMN')
-plt.ylabel('ROW')
-plt.show()
-print('Location Prediction' + str(PIXELS_OUT))
-
-print('TEST 5: ')
-objRowCol = [512, 512]
-frame_t1_ecef = np.asarray([0, 0, 1000])
-frame_t2_ecef = np.asarray([0, 10, 1000])
-LOS = np.asarray([5, 10, 5])
-LatLon = [90, 0]
-PIXELS_OUT = locModel. objectLocationPredictor(objRowCol, frame_t1_ecef, frame_t2_ecef, LOS, LatLon)
-plt.figure()
-v = [1, 1024, 1, 1024]
-plt.axis(v)
-# maintaining camera coordinate system
-plt.ylim(1024,1)
-plt.scatter(objRowCol[1], objRowCol[0], color = 'b')
-plt.scatter(PIXELS_OUT[1], PIXELS_OUT[0], color = 'r')
-plt.legend(['Object t1', 'Object t2'])
-plt.xlabel('COLUMN')
-plt.ylabel('ROW')
-plt.show()
-print('Location Prediction' + str(PIXELS_OUT))
