975512e8e9dc58c4b40b1460d2443e7310fd69ff
jmaxmarno <jmaxmarno@gmail.com>
2019-04-29 21:25:56 -0600

    Basic Click

--- Src/ClickObject/BasicClick.py
+++ Src/ClickObject/BasicClick.py
@@ -5,6 +5,8 @@ Created on Tue Apr 16 20:09:17 2019
 @author: Max Marno
 """
 
+coords = list()
+
 import matplotlib.pyplot as plt
 
 im = plt.imread(r"C:\Users\Max Marno\Documents\Projects\GSV\TESTIMAGE.png")
@@ -15,6 +17,7 @@ implot = ax.imshow(im)
 def onclick(event):
     if event.xdata != None and event.ydata != None:
         print(event.xdata, event.ydata)
+
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
 plt.show()
