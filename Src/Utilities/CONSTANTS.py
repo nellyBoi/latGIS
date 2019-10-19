79dd9b22981de13bf1d4358fdc86c619c1904947
John Kane <john.n.kane@colorado.edu>
2019-10-19 01:01:09 -0600

    Some renaming

--- Src/Utilities/CONSTANTS.py
+++ Src/Utilities/CONSTANTS.py
@@ -6,12 +6,14 @@ Dec.08.2018
 
 This script will hold constants used by multiple routines in latGIS.
 
+TODO: Change to python config file
+
 '''
 import numpy as np
 
 # camera/sensor parameters
-class constants:
+class Constants:
     FOV = 30 # degrees
-    sensorSize = [1024, 1024]
-    rad2deg = 180/np.pi
-    deg2rad = np.pi/180
+    SENSOR_SIZE = [1024, 1024] # TODO Change
+    RAD2DEG = 180/np.pi
+    DEG2RAD = np.pi/180
