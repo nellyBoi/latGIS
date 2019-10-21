41272d9e029e2c59f907fb99088544de08779c9d
John Kane <john.n.kane@colorado.edu>
2019-10-21 01:23:51 -0600

    using unittest for triangulate

--- algorithm/test/latgis/triangulate_unitTest.py
+++ algorithm/test/latgis/triangulate_unitTest.py
@@ -22,82 +22,101 @@ All outputs are hand-checked. A vaerification source should be used at a
 later point. 
 
 """
+import unittest
+
+from numpy import array, ndarray, abs
+
 from latgis.util.triangulate import minDistPoint_3D
-from numpy import array
 
-# parallel lines
-print('Triangulation: Unit Test 1')
-lineDir1 = array([0, 2, 0])
-linePt1 = array([1, 0, 0])
-lineDir2 = array([0, 1, 0])
-linePt2 = array([5, 0, 0])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
-minDistCalc = 4
-minPtCalc = array([0, 0, 0])
-print('Closest point Error: ' + str(minPt - minPtCalc)\
-      + '\n Minimum Distance Error: ' + str(minDist-minDistCalc), '\n')
-
-# skew lines separated by a distance (in  XY-plane)
-print('Triangulation: Unit Test 2')
-lineDir1 = array([2, 1, 0])
-linePt1 = array([0, 0, 0])
-lineDir2 = array([-1, 1, 0])
-linePt2 = array([3, 0, 0])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
-minDistCalc = 0
-minPtCalc = array([2, 1, 0])
-print('Closest point Error: ' + str(minPt - minPtCalc)\
-      + '\n Minimum Distance Error: ' + str(minDist-minDistCalc) + '\n')
-
-# skew lines separated by a distance (in XZ-plane)
-print('Triangulation: Unit Test 3')
-lineDir1 = array([1, 0, 2])
-linePt1 = array([-4, 0, -2])
-lineDir2 = array([-1, 0, 0])
-linePt2 = array([5, 0, 2])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
-minDistCalc = 0
-minPtCalc = array([-2, 0, 2])
-print('Closest point Error: ' + str(minPt - minPtCalc)\
-      + '\n Minimum Distance Error: ' + str(minDist-minDistCalc) + '\n')
-
-# skew lines separated by a distance (in YZ-plane)
-print('Triangulation: Unit Test 4')
-lineDir1 = array([0, 1, -1])
-linePt1 = array([0, -2, 3])
-lineDir2 = array([0, -1, -1])
-linePt2 = array([0, 2, 3])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
-minDistCalc = 0
-minPtCalc = array([0, 0, 1])
-print('Closest point Error: ' + str(minPt - minPtCalc)\
-      + '\n Minimum Distance Error: ' + str(minDist-minDistCalc) + '\n')
-
-# skew lines separated by a distance (in 3D space)
-print('Triangulation: Unit Test 5')
-lineDir1 = array([-2, 0, 1])
-linePt1 = array([3, 2, 0])
-lineDir2 = array([5, 0, 1])
-linePt2 = array([-4, 0, 0])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
-minDistCalc = 2
-minPtCalc = array([1, 1, 1])
-print('Closest point Error: ' + str(minPt - minPtCalc)\
-      + '\n Minimum Distance Error: ' + str(minDist-minDistCalc) + '\n')
-
-# skew lines in 2D, intersect opposite of line of travel (intersection found 
-# to be behind camera)
-print('Triangulation: Unit Test 6')
-lineDir1 = array([1, 1, 0])
-linePt1 = array([3, 0, 0])
-lineDir2 = array([-1, 1, 1])
-linePt2 = array([1, 0, 0])
-[minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+DOUBLE_TOL = 1e-5
 
 
+########################################################################################################################
 
+########################################################################################################################
+class TriangulateTestCase(unittest.TestCase):
 
+    def assertEqualsOnVector(self, vector: ndarray):
+        """
+        :param vector: input vector
+        :return: pass if all components are smaller than DOUBLE_TOL.
+        """
+        self.assertTrue(abs(vector[0]) < DOUBLE_TOL)
 
+    def test_parallel_lines(self):
+        print('Triangulation: Unit Test 1')
+        lineDir1 = array([0, 2, 0])
+        linePt1 = array([1, 0, 0])
+        lineDir2 = array([0, 1, 0])
+        linePt2 = array([5, 0, 0])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        minDistCalc = 4
+        minPtCalc = array([0, 0, 0])
+        self.assertTrue(abs(minDist - minDistCalc) < DOUBLE_TOL)
+        self.assertEqualsOnVector(minPt - minPtCalc)
 
+    def test_2(self):
+        # skew lines separated by a distance (in  XY-plane)
+        print('Triangulation: Unit Test 2')
+        lineDir1 = array([2, 1, 0])
+        linePt1 = array([0, 0, 0])
+        lineDir2 = array([-1, 1, 0])
+        linePt2 = array([3, 0, 0])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        minDistCalc = 0
+        minPtCalc = array([2, 1, 0])
+        self.assertTrue(abs(minDist - minDistCalc) < DOUBLE_TOL)
+        self.assertEqualsOnVector(minPt - minPtCalc)
 
+    def test_3(self):
+        # skew lines separated by a distance (in XZ-plane)
+        print('Triangulation: Unit Test 3')
+        lineDir1 = array([1, 0, 2])
+        linePt1 = array([-4, 0, -2])
+        lineDir2 = array([-1, 0, 0])
+        linePt2 = array([5, 0, 2])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        minDistCalc = 0
+        minPtCalc = array([-2, 0, 2])
+        self.assertTrue(abs(minDist - minDistCalc) < DOUBLE_TOL)
+        self.assertEqualsOnVector(minPt - minPtCalc)
 
+    def test_4(self):
+        # skew lines separated by a distance (in YZ-plane)
+        print('Triangulation: Unit Test 4')
+        lineDir1 = array([0, 1, -1])
+        linePt1 = array([0, -2, 3])
+        lineDir2 = array([0, -1, -1])
+        linePt2 = array([0, 2, 3])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        minDistCalc = 0
+        minPtCalc = array([0, 0, 1])
+        self.assertTrue(abs(minDist - minDistCalc) < DOUBLE_TOL)
+        self.assertEqualsOnVector(minPt - minPtCalc)
+
+    def test_5(self):
+        # skew lines separated by a distance (in 3D space)
+        print('Triangulation: Unit Test 5')
+        lineDir1 = array([-2, 0, 1])
+        linePt1 = array([3, 2, 0])
+        lineDir2 = array([5, 0, 1])
+        linePt2 = array([-4, 0, 0])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        minDistCalc = 2
+        minPtCalc = array([1, 1, 1])
+        self.assertTrue(abs(minDist - minDistCalc) < DOUBLE_TOL)
+        self.assertEqualsOnVector(minPt - minPtCalc)
+
+    def test_6(self):
+        # skew lines in 2D, intersect opposite of line of travel (intersection found
+        # to be behind camera)
+        print('Triangulation: Unit Test 6')
+        lineDir1 = array([1, 1, 0])
+        linePt1 = array([3, 0, 0])
+        lineDir2 = array([-1, 1, 1])
+        linePt2 = array([1, 0, 0])
+        [minPt, minDist] = minDistPoint_3D(lineDir1, linePt1, lineDir2, linePt2)
+        print('should be nan : ' + str(minPt) + ' ,    ' + str(minDist))
+
+if __name__ == '__main__':
+    unittest.main()
