7958c3194fd2c44b628c7b6c550f542f1b4ae5e9
nellyBoi <johnnelsonkane@Johns-iMac.local>
2020-09-16 18:12:01 -0600

    main driver started

---
+++ runLatGIS.py
@@ -0,0 +1,28 @@
+
+
+# add all necessary code to the path
+import os
+import sys
+
+PYTHON_PATH_ADDS = ['algorithm', 'data_collect', 'gui']
+
+
+########################################################################################################################
+def setup() -> None:
+    file_path = os.path.dirname(os.path.realpath(__file__))
+
+    for folder in PYTHON_PATH_ADDS:
+        full_path = os.path.join(file_path, folder)
+        sub_paths = os.walk(full_path)
+        for subPath in sub_paths:
+
+            if 'pycache' not in subPath:
+                sys.path.extend(subPath)
+
+
+########################################################################################################################
+if __name__ == '__main__':
+    setup()
+    from gui import run_ipview
+    run_ipview.run()
+
